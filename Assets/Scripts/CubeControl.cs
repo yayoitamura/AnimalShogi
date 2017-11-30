0142aa7b18bebb5bc3e45077c9506b78864bc0b4
yayoitamura <gh3520900@gmail.com>
2017-11-30 23:26:45 +0900

    Use EventTrigger

---
+++ Assets/Scripts/CubeControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class CubeControl : MonoBehaviour {
+
+	Rigidbody rigidBody;
+	public Vector3 force = new Vector3 (0, 10, 0);
+	public ForceMode forceMode = ForceMode.VelocityChange;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = gameObject.GetComponent<Rigidbody> ();
+
+	}
+	public void OnUserAction () {
+		rigidBody.AddForce (force, forceMode);
+		Debug.Log ("OnUserAction");
+	}
+}
