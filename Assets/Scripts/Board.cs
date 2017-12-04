724ab259e868841e35bd401a8ad3ecc297d4b119
yayoitamura <gh3520900@gmail.com>
2017-12-04 23:51:56 +0900

    Move function

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+	string[, ] board = new string[3, 4];
+
+	// Use this for initialization
+	void Start () {
+		board[1, 2] = "hiyoko";
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnUserAction (CubeControl koma) {
+		koma.OnUserAction ();
+		// Vector2 pos = GetComponent<RectTransform> ().anchoredPosition;
+		// pos.y += -220;
+		// GetComponent<RectTransform> ().anchoredPosition = pos;
+
+	}
+}
